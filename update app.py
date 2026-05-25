1a001581a0afe7be00a14ce3adb92c61a305d71f
jahnavi-ramanji <jahnuram029@gmail.com>
2026-05-25 19:37:47 +0530

    Update update app.py

--- update app.py
+++ update app.py
@@ -32,10 +32,8 @@ st.set_page_config(
 # ---------------------------------------------------
 st.markdown("""
 <style>
-/* Hide Sidebar Collapse Button */
-[data-testid="collapsedControl"] {
-    display: none;
-}
+
+
 
 /* Main App */
 .stApp {
